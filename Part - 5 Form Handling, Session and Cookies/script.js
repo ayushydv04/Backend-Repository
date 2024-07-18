b43c6d4a4abd5e8439088fc5abc854c921943053
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-07-18 20:15:33 +0530

    commit

---
+++ Part - 5 Form Handling, Session and Cookies/script.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+// Middleware
+app.use((req, res, next)=>{
+    console.log("Middleware chala");
+    next();
+});
+
+
+app.get('/', (req, res)=>{
+    res.send("Hello World");
+});
+
+app.get('/profile', (req, res)=>{
+    res.send("This is profile page");
+})
+
+app.get('/about', (req, res)=>{
+    res.send("About page");
+});
+
+app.get('/contact', (req, res)=>{
+    res.send("Contact Information")
+});
+
+app.get('/register', (req, res)=>{  
+    return next(new Error("Something went wrong"));
+});
+
+
+// Error handling
+app.use((err, req, res, next)=>{
+    console.error(err.stack);
+    res.status(500).send("404 page not found!");
+}); 
+
+
+app.listen(3000);
